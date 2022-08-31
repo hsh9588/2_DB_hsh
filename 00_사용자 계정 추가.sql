b0e0eccbeea8646725548fcde9abc800a374b4b0
hsh9588 <hsh9588@naver.com>
2022-08-31 17:47:53 +0900

    Initial commit

---
+++ 00_사용자 계정 추가.sql
@@ -0,0 +1,21 @@
+-- 한 줄 주석
+
+/*
+ * 범위 주석 동일 
+ */ 
+
+-- 계정 만드는 코드
+
+-- SQL 하나 수행 : CTRL + ENTER
+ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
+
+
+-- 실습용 사용자 계정 생성
+CREATE USER kh_hsh IDENTIFIED BY kh1234;
+
+-- 사용자 계정 권한 부여
+GRANT RESOURCE, CONNECT TO kh_hsh;
+
+-- 객체 생성(테이블 등) 공간 할당량 지정
+ALTER USER kh_hsh DEFAULT TABLESPACE SYSTEM
+QUOTA UNLIMITED ON SYSTEM;
